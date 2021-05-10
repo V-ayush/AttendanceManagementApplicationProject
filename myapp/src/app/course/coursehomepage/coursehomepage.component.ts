6055a6bc8f0fe5e2e1a188b62926add03708b5cc
V-ayush <vayush234@gmail.com>
2021-05-11 03:28:36 +0530

    commit

---
+++ myapp/src/app/course/coursehomepage/coursehomepage.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-coursehomepage',
+  templateUrl: './coursehomepage.component.html',
+  styleUrls: ['./coursehomepage.component.css']
+})
+export class CoursehomepageComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToCourseList(){
+    this.router.navigate(['courses']);
+  }
+  goToAddCourse(){
+    this.router.navigate(['create-course']);
+  }
+
+}
---
+++ myapp/src/app/subject/home-page/home-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class SHomePageComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+  goToAddSub()
+  {
+    this.router.navigate(['create-subject'])
+  }
+  goToHomePage(){
+    this.router.navigate(['home-page'])
+  }
+  goToSubjectList(){
+    this.router.navigate(['/subject-list']);
+  }
+}
